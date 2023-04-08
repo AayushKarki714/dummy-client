fd38e38472edc1d3590dc8a64107ceff6bee76d9
Aayush <karkiayush201@gmail.com>
2023-04-08 22:51:28 +0545

    second commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,72 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import { useEffect } from "react";
 
 function App() {
+  const [userName, setUserName] = useState("");
+  const [users, setUsers] = useState([]);
+  const [email, setEmail] = useState("");
+  const [isStale, setIsStale] = useState(true);
+
+  useEffect(() => {
+    fetch("https://dumm-server.onrender.com/users")
+      .then((res) => res.json())
+      .then((data) => {
+        setUsers(data?.users);
+      });
+  }, [isStale]);
+
+  const handleFormSubmit = async (event) => {
+    event.preventDefault();
+
+    if (!userName || !email) return;
+    console.log({ userName, email });
+
+    fetch("https://dumm-server.onrender.com/user", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ userName, email }),
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        console.log({ data });
+        setIsStale(!isStale);
+      });
+  };
+
+  console.log({ users });
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <form onSubmit={handleFormSubmit}>
+        <div>
+          <label htmlFor="username">Username:</label>
+          <input
+            type="text"
+            value={userName}
+            onChange={(event) => setUserName(event.target.value)}
+          />
+        </div>
+
+        <div>
+          <label htmlFor="email">Email:</label>
+          <input
+            type="email"
+            value={email}
+            onChange={(event) => setEmail(event.target.value)}
+          />
+        </div>
+        <button type="submit">Submit</button>
+      </form>
+      <div>
+        {users.map(({ id, userName, email }) => (
+          <div>
+            <h2>{userName}</h2>
+            <p>{email}</p>
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
